8e5fb82857459f7b2fc2961a1f84224d38b8d3dc
ifritzord <88744@studenti.unimore.it>
2020-12-30 19:58:27 +0100

    user endpoint & test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,9 +1,15 @@
 const request =  require('supertest')
 const server =  require('../bin/www')
 
-describe('Get Endpoints', () => {
+describe('Endpoints Checks', () => {
     it('Get Home page', async (done) => {
         const res =  await request(server).get('/');
+        expect(res.statusCode).toEqual(200);
+        done();
+    });
+
+    it('Get Users list', async (done) => {
+        const res =  await request(server).get('/users');
         expect(res.statusCode).toEqual(200);
         done();
     })
